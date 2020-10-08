e36bd728d44392cacdc6f25bff27c4f9fe6fb32c
hedma <johannes91.hedman@gmail.com>
2020-10-08 09:47:46 +0200

    Added Volatile for JVM, reflecting and Race Condition protection

--- src/creational/singleton/DBSingleton.java
+++ src/creational/singleton/DBSingleton.java
@@ -2,17 +2,25 @@ package creational.singleton;
 
 public class DBSingleton {
 
-	private static DBSingleton instance = null;
+	// Volatile will assure the JVM to keep the instance a Singleton
+	private static volatile DBSingleton instance = null;
 
 	private DBSingleton() {
-		
+		// This will prevent a reflection class from creating an instance
+		if (instance != null) {
+			throw new RuntimeException("Use getInstance() method to create");
+		}
 	}
-	
+
 	public static DBSingleton getInstance() {
-		if(instance == null) {
-			instance = new DBSingleton();
+		if (instance == null) {
+			// This will prevent two threads colliding
+			synchronized (DBSingleton.class) {
+				if (instance == null)
+					instance = new DBSingleton();
+			}
 		}
 		return instance;
 	}
-	
+
 }
