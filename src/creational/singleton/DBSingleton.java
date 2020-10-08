df2f289aac1f3a863b7056c7a5b57f2596e7112a
hedma <johannes91.hedman@gmail.com>
2020-10-08 09:41:03 +0200

    Added Lazy Loading

--- src/creational/singleton/DBSingleton.java
+++ src/creational/singleton/DBSingleton.java
@@ -2,13 +2,16 @@ package creational.singleton;
 
 public class DBSingleton {
 
-	private static DBSingleton instance = new DBSingleton();
+	private static DBSingleton instance = null;
 
 	private DBSingleton() {
 		
 	}
 	
 	public static DBSingleton getInstance() {
+		if(instance == null) {
+			instance = new DBSingleton();
+		}
 		return instance;
 	}
 	
